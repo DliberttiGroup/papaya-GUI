3584e454016ceed5b02a5dd05b527753047ad097
DestroK503 <105082321+destroK503@users.noreply.github.com>
2024-08-12 20:42:42 -0600

    [Early work] Wrote the contructor of the class

--- lib/src/main/java/com/papaya/WindowManager.java
+++ lib/src/main/java/com/papaya/WindowManager.java
@@ -5,19 +5,81 @@
  */
 package com.papaya;
 
-import utils.testLib.debug;
+import utils.textLib.debug;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.util.HashMap;
+
+import javax.swing.UIManager;
+import javax.swing.JFrame;
 
 /**
  * WindowManager
  */
 public class WindowManager {
+  /**
+   * Window manager class
+   *
+   *
+   * @DESCRIPTION
+   *
+   *              This class is a high level actraction of
+   *              the swing API
+   *
+   * @Note
+   *       You can call `returnJframe` method
+   *       so you can work with the JFrame object
+   *       directly
+   *
+   */
+  private static JFrame f;
 
-  public WindowManager() {
-    debug.info("instance Created");
+  /*
+   * This constructor will create an empty window
+   */
+  public WindowManager(String nameOfYourProgram) {
+      HashMap<String, Integer> screenSize = getDimensionsScreen();
+
+      try {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+
+      f = new JFrame(nameOfYourProgram);
+      f.setSize(screenSize.get("width") / 2, screenSize.get("heigh") / 2);
+
+      f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      f.setLocationRelativeTo(null);
+
+      f.setVisible(true);
+      debug.info(nameOfYourProgram + " " + "Is Up And Running!");
+    }
+
+  /*
+   * Returns the JFrame object
+   */
+  public JFrame returnJframe() {
+    debug.info("Frame returned");
+    return f;
   }
 
-  public static void holla(String txt){
-    debug.error(txt);
+  /*
+   * Gets the the size of the current
+   * screen
+   */
+  public static HashMap<String, Integer> getDimensionsScreen() {
+    Toolkit toolkit = Toolkit.getDefaultToolkit();
+    HashMap<String, Integer> screenValues = new HashMap<>();
+
+    Dimension screenSize = toolkit.getScreenSize();
+    int width = screenSize.width;
+    int heigh = screenSize.height;
+
+    screenValues.put("width", width);
+    screenValues.put("heigh", heigh);
+
+    return screenValues;
   }
 
 }
